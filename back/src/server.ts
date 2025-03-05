04a1698e0dffe065c33c19745ec94e1786766311
M-R-Kuen <157444501+M-R-Kuen@users.noreply.github.com>
2025-03-05 18:36:13 -0300

    update cors

--- back/src/server.ts
+++ back/src/server.ts
@@ -4,8 +4,14 @@ import router from "./routes";
 import morgan from "morgan";
 
 const app = express();
+const corsOptions = {
+  origin: "https://neanderthal-outdoors.vercel.app", // Reemplaza con la URL de tu frontend
+  methods: ["GET", "POST", "PUT", "DELETE"], // Especifica los métodos permitidos si es necesario
+  credentials: true, // Si necesitas enviar cookies o sesiones, habilita esto
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
+//app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 

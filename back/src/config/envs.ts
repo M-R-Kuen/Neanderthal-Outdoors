4809cf8ca57295a2738d6e14da2c4be47a19da4f
M-R-Kuen <157444501+M-R-Kuen@users.noreply.github.com>
2025-03-05 17:52:35 -0300

    updates render

--- back/src/config/envs.ts
+++ back/src/config/envs.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 export const PORT: number = Number(process.env.PORT) || 3000;
 export const DB_NAME: string = process.env.DB_NAME || "proyecto_m4_front";
-export const DB_USER: string = process.env.DB_USER || "rosaura";
+export const DB_USER: string = process.env.DB_USERNAME || "rosaura";
 export const DB_PASSWORD: string = process.env.DB_PASSWORD || "admin";
 export const DB_HOST: string = process.env.DB_HOST || "localhost";
 export const DB_PORT: number = Number(process.env.DB_PORT) || 5432;
